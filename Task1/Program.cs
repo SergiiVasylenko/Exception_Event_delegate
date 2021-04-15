82b4babf61b52fa713f5b11bfbf2013e9894e733
sergii <servasylenko@deloitte.com>
2021-04-15 12:43:40 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var exceptionArray = new Exception[] { new ArgumentException(), new ArgumentOutOfRangeException(), new DivideByZeroException(), 
+                                 new FormatException(), new UserException("My exception") };
+
+            foreach (var item in exceptionArray)
+            {
+                try
+                {
+                    throw item;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class UserException : Exception
+    {
+        public UserException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<string> { "Корнеев", "Федеров", "Яшин", "Акулов", "Игнатов" };
+
+            bool isFalseNumber = true;
+
+            while (isFalseNumber)
+            {
+                try
+                {
+                    Console.Write("Enter the 1 or 2: ");
+                    var str = Console.ReadLine();
+                    var result = int.TryParse(str, out int number);
+                    if (!result || (number != 1 && number != 2))
+                    {
+                        throw new MyException("Введите цифру один или два");
+                    }
+                    else
+                    {
+                        var user = new UserNumber();
+                        user.CompletedEnter += Sort;
+                        user.Start(persons, number);
+                        isFalseNumber = false;
+                    }
+                }
+                catch (MyException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    isFalseNumber = true;
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static void Sort(List<string> persons, int number)
+        {
+            var sortedPerson = number == 1 ? persons.OrderBy(x => x) : persons.OrderByDescending(x => x);
+
+            foreach (var item in sortedPerson)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    class UserNumber
+    {
+        public delegate void UserEntered(List<string> persons, int number);
+        public event UserEntered CompletedEnter;
+
+        public void Start(List<string> persons, int number)
+        {
+            OnProccessCompleted(persons, number);
+        }
+
+        protected void OnProccessCompleted(List<string> persons, int number)
+        {
+            CompletedEnter?.Invoke(persons, number);
+        }
+
+    }
+
+    class MyException : Exception
+    {
+        public MyException(string message) : base(message)
+        {
+
+        }
+    }
+}
